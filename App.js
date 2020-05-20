68d9127a82b10b872cad9d0f114d4710db4f0865
Lautaro Martinez <59697136+LauMartinez-Code@users.noreply.github.com>
2020-05-20 01:44:17 -0300

    Actividades 1-4 realizadas

--- App.js
+++ App.js
@@ -1,36 +1,42 @@
-/*let sayHello = (name) => {
-    console.log('Hello ' + name);
-};
-
-sayHello('Lautarot');*/
-
 const Logger = require('./Logger');
 const path =  require('path');
 const os = require('os');
 const fs = require('fs');
 
-//console.log(Logger);
-//Logger.log('Lautarot');
-
-let pathObj = path.parse(__filename);
-
-Logger.log(pathObj);
-
-let freeMem = os.freemem();
+/* let freeMem = os.freemem();
 let totalMem = os.totalmem();
-
 //Logger.log('free ', freeMem);
 console.log('free: ', freeMem);
-console.log('total: ', totalMem);
+console.log('total: ', totalMem); */
 
-let rootDir = fs.readdirSync('./');
-//console.log(rootDir);
+//1 - Create a method to get all the files inside a given path
+console.log('\n\t\t**EJER 1**');
 
-fs.readdir('./', (err, files) => {
-    (err) ? console.log(err) : console.log('async', files);
+let pathObj = path.parse(__filename);
+Logger.log(pathObj);
+
+//2 - Create a method to Print all the files
+console.log('\n\t\t**EJER 2**');
+let filesOfPath = [];
+
+filesOfPath =  fs.readdirSync('./', (err, files) => {               //readdirSync() es 'sync y async es readdir()  se obtiene el resultado primero y el otro despues
+    (err) ? console.log(err) : console.log('Files on path', files)
 });
 
-console.log('sync', rootDir); //se obtiene el resultado primero
+console.log('Las files del dir', pathObj.dir,'son: ', filesOfPath);
 
 
+//3 - Create a method to sort and print the files
+console.log('\n\t\t**EJER 3**');
+console.log('Las files en orden inverso: ', filesOfPath.reverse());
+
+//4 - Create a method to get the amount of files starting with one letter
+console.log('\n\t\t**EJER 4**');
+let count = 0;
+
+filesOfPath.forEach(Element => {
+    Element.startsWith('A') ? count++ : {};
+});
+
+console.log(count,'files comienzan con la letra \'A\'');
 
--- Logger.js
+++ Logger.js
@@ -1,8 +1,9 @@
 let url = 'http://myurl.com';
 
 log = (message) => {
-    console.log('Hello ', message);
+    console.log('Data of file: ',message.base, message);
 };
+
 
 //module.exports.url = url;
 module.exports.log = log;
